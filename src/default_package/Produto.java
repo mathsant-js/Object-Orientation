5de08676bb937d30de59b3059d78168bba09a597
matheussantana2007 <matheus.santana114@etec.sp.gov.br>
2024-06-10 22:21:55 -0300

    chore: project structure

---
+++ src/default_package/Produto.java
@@ -0,0 +1,17 @@
+// Classes
+
+/*
+ * Os nomes das classes iniciam com letra maiúscula
+ * O nome não deve conter: Acentuação, caracteres especiais, espaço
+ * Nas classes Java, não existe a implementação da função main()
+ * Toda classe Java possui a seguinte forma:
+ * 
+ * public class NomeDaClasse {
+ * 
+ * } 
+ */
+package default_package;
+
+public class Produto {
+
+}
